da2ddfb9f8d83693930ea7cc80c2d19c2a0f4b48
EugeneSemivolos <semivolos.evgen1@gmail.com>
2023-06-07 19:45:21 +0300

    implemented replaceByRegExp() function

---
+++ src/replace.js
@@ -0,0 +1,29 @@
+const { matchExpr, isStart, removeSlashWrapper } = require('./utils');
+
+function replaceByRegExp(inputExpr, word, str) {
+  if (inputExpr.length === 0 || inputExpr === '//') throw new Error('There is no RegExp. To solve it enter the RegExp');
+  if (str.length === 0) throw new Error('Text to search is empty. To solve this problem enter the text');
+  
+  let expr = removeSlashWrapper(inputExpr);
+  let resStr = str;
+  
+  if (isStart(expr[0])) {
+    expr = expr.slice(1);
+    const [isMatched, length] =  matchExpr(expr, resStr);
+    if (isMatched) {
+      resStr = word + resStr.slice(length);
+    }
+    return resStr;
+  }
+
+  for (let pos = 0; pos < resStr.length; pos++) {
+    const [isMatched, length] = matchExpr(expr, resStr.slice(pos));
+    if (isMatched) {
+      resStr = resStr.slice(0, pos) + word + resStr.slice(pos + length);
+      pos += word.length;
+    }
+  }
+  return resStr;
+}
+
+module.exports = { replaceByRegExp };
