0c6911a9c7a9d20d84f749c2afab57c5ccbc88c6
EugeneSemivolos <semivolos.evgen1@gmail.com>
2023-06-08 10:44:47 +0300

    added tests for CLParser

--- src/CLParser.js
+++ src/CLParser.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 class CLParser {
   
   static getCommand(args) {
-    return args[2];
+    if (args[2]) return args[2];
+    else throw new Error("Enter command (match/replace)")
   }
   
   static getRegExp(args) {
@@ -12,7 +13,14 @@ class CLParser {
   }
 
   static getInputText(args) {
-    const field = args[2] === 'replace' ? args[5] : args[4];
+    const cmd = this.getCommand(args);
+    let field;
+    if (cmd === 'replace') {
+      field = args[5];
+    } else if (cmd === 'match') {
+      if (args[5]) throw new Error("When command is match, there is only regexp and text to search.");
+      field = args[4];
+    }
     if (!field) throw new Error("Enter the path to file or the text to search");
     try {
       const fileText = fs.readFileSync(field).toString();
@@ -27,7 +35,6 @@ class CLParser {
   }
 
   static parseCommandLine(args) {
-    console.log(args);
     const cmd = this.getCommand(args);
     const regExp = this.getRegExp(args);
     const textToReplace = this.getTextToReplace(args);
--- tests/CLParser/CLParser.test.js
+++ tests/CLParser/CLParser.test.js
@@ -3,40 +3,66 @@ var path = require("path");
 
 describe('Testing CLIParser', () => {
   test('No arguments', () => {
-    const data = ['npm','start'];
+    const data = ['npm','run'];
     expect(() => {
-      CLParser.getRegExp(data);
-    }).toThrow();
-    expect(() => {
-      CLParser.getText(data);
-    }).toThrow();
-
-    const data2 = ['npm','start', '', ''];
-    expect(() => {
-      CLParser.getRegExp(data2);
-    }).toThrow();
-    expect(() => {
-      CLParser.getText(data2);
-    }).toThrow();
+      CLParser.parseCommandLine(data);
+    }).toThrow('Enter command');
   });
 
-  test('Only one argument', () => {
-    const data = ['npm','start', '/regexp/'];
-    expect(CLParser.getRegExp(data)).toMatch('/regexp/');
+  test('Only command', () => {
+    const data = ['npm','run', 'match'];
     expect(() => {
-      CLParser.getText(data);
-    }).toThrow();
+      CLParser.parseCommandLine(data);
+    }).toThrow('Enter the regular Expression');
   });
 
-  test('Second argument is the path', () => {
-    const data = ['npm','start', '/regexp/', `${path.join(__dirname, 'test.txt')}`];
-    expect(CLParser.getRegExp(data)).toMatch('/regexp/');
-    expect(CLParser.getText(data)).toMatch('Hello world!');
+  test('Only command and RegExp', () => {
+    const data = ['npm','run', 'match', '/find/'];
+    expect(() => {
+      CLParser.parseCommandLine(data);
+    }).toThrow('Enter the path to file or the text to search');
   });
 
-  test('Second argument is a text', () => {
-    const data = ['npm','start', '/regexp/', 'Find in me!'];
-    expect(CLParser.getRegExp(data)).toMatch('/regexp/');
-    expect(CLParser.getText(data)).toMatch('Find in me!');
+  test('There is text to replace but command is match', () => {
+    const data = ['npm','run', 'match', '/find/', 'REPLACE', 'This is text to find smth'];
+    expect(() => {
+      CLParser.parseCommandLine(data);
+    }).toThrow('When command is match, there is only regexp and text to search.');
+  });
+
+  test('There is no text to replace but command is replace', () => {
+    const data = ['npm','run', 'replace', '/find/', 'This is text to find smth'];
+    expect(() => {
+      CLParser.parseCommandLine(data);
+    }).toThrow('Enter the path to file or the text to search');
+  });
+
+  test('Test match function', () => {
+    const data = ['npm','run', 'match', '/[pc]o+le*/', 'Try to find pool. You are cool. What is pole?'];
+    
+    const { cmd, regExp, inputText } = CLParser.parseCommandLine(data);
+    expect(cmd).toBe('match');
+    expect(regExp).toBe('/[pc]o+le*/');
+    expect(inputText).toBe('Try to find pool. You are cool. What is pole?');
+  });
+
+  test('Test replace function', () => {
+    const data = ['npm','run', 'replace', '/[pc]o+le*/', 'smth', 'Try to find pool. You are cool. What is pole?'];
+    
+    const { cmd, regExp, textToReplace, inputText } = CLParser.parseCommandLine(data);
+    expect(cmd).toBe('replace');
+    expect(regExp).toBe('/[pc]o+le*/');
+    expect(textToReplace).toBe('smth');
+    expect(inputText).toBe('Try to find pool. You are cool. What is pole?');
+  });
+
+  test('Test path to file where search is needed', () => {
+    const data = ['npm','run', 'match', '/[pc]o+le*/', './tests/CLParser/test.txt'];
+    
+    const { cmd, regExp, textToReplace, inputText } = CLParser.parseCommandLine(data);
+    expect(cmd).toBe('match');
+    expect(regExp).toBe('/[pc]o+le*/');
+    expect(textToReplace).toBe('');
+    expect(inputText).toBe('Hello world!');
   });
 });
