fd8e0509a630a2bf7275aef2d87cf7f1fa6ea370
EugeneSemivolos <semivolos.evgen1@gmail.com>
2023-06-08 10:15:40 +0300

    Match and replace functions are integrated into the main file

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const CLParser = require("./src/CLParser");
+const { matchByRegExp } = require("./src/match");
+const { replaceByRegExp } = require("./src/replace");
 
 function main() {
-  const regExp = CLParser.getRegExp(process.argv);
-  const text = CLParser.getText(process.argv)
-  console.log(regExp, "\n", text);
+  const { cmd, regExp, textToReplace, inputText } = CLParser.parseCommandLine(process.argv);
+
+  if (cmd === 'match') {
+    const matchList = matchByRegExp(regExp, inputText);
+    for (const [i, item] of matchList.entries()) {
+      const [pos, length] = item;
+      const matchedStr = inputText.slice(pos, pos + length);
+      console.log(`${i + 1} match: ${matchedStr}`);
+    }
+  } else if (cmd === 'replace') {
+    const replacedStr = replaceByRegExp(regExp, textToReplace, inputText);
+    console.log(replacedStr);
+  } else {
+    return console.log('There is no command you sent');
+  }
 }
 main();
--- src/CLParser.js
+++ src/CLParser.js
@@ -1,13 +1,18 @@
 const fs = require('fs');
 
 class CLParser {
-  static getRegExp(args) {
-    if (!args[2]) throw new Error("Enter the regular Expression");
+  
+  static getCommand(args) {
     return args[2];
   }
+  
+  static getRegExp(args) {
+    if (!args[3]) throw new Error("Enter the regular Expression");
+    return args[3];
+  }
 
-  static getText(args) {
-    const field = args[3];
+  static getInputText(args) {
+    const field = args[2] === 'replace' ? args[5] : args[4];
     if (!field) throw new Error("Enter the path to file or the text to search");
     try {
       const fileText = fs.readFileSync(field).toString();
@@ -15,6 +20,20 @@ class CLParser {
     } catch {
       return field;
     }
+  }
+
+  static getTextToReplace(args) {
+    return args[2] === 'replace' ? args[4] : '';
+  }
+
+  static parseCommandLine(args) {
+    console.log(args);
+    const cmd = this.getCommand(args);
+    const regExp = this.getRegExp(args);
+    const textToReplace = this.getTextToReplace(args);
+    const inputText = this.getInputText(args);
+
+    return { cmd, regExp, textToReplace, inputText };
   }
 }
 
